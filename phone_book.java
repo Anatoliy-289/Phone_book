9cd129f1db9b751b7ed5e3fefe9c8eeaedd732a8
Anatoliy Turanskiy <anatol911@yandex.ru>
2024-06-15 20:11:10 +0500

    Создали класс phone_book

---
+++ phone_book.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+
+public class phone_book{
+    
+    private String name;
+    private String surname;
+    private String second_name;
+    private LocalDate birthDate;
+    private String status;
+    private String place_of_residence;
+    private String place_of_work;
+    private String comments;
+
+}

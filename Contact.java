60ed7d39bdaa6722afccebd36e8fe18997c5c568
Anatoliy Turanskiy <anatol911@yandex.ru>
2024-06-16 20:22:00 +0500

    Создали класс Contact

---
+++ Contact.java
@@ -0,0 +1,21 @@
+public class Contact {
+    
+    private List<Phone_book> = new ArrayList<>();
+
+    public Contact(List<Phone_book> contacts){
+        this.contacts = contacts;
+
+    }
+
+    public void initContacts(List<Phone_book> contacts){
+        this.contacts.addAll(contacts);
+    }
+
+    public void addContacts(List<Phone_book> contacts){
+        this.contacts.addAll(contacts);
+    }
+
+
+
+
+}
